005a7214a311b92710b7bfcd4f6ee3a13b54de3b
João Pedro Silva Souza Nunes <126632669+Joao-Pedro12@users.noreply.github.com>
2023-04-24 20:12:25 -0300

    Add files via upload

---
+++ Matriz1.py
@@ -0,0 +1,31 @@
+#Matriz 3x3
+matriz_3x3 = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+# Imprimindo cada lista
+print(matriz_3x3[0])
+print(matriz_3x3[1])
+print(matriz_3x3[2])
+# Imprimindo um item da lista
+print(matriz_3x3[0][1])
+print(matriz_3x3[1][1])
+print(matriz_3x3[2][1])
+
+# Matriz 5x5
+matriz_5x5 = [
+    [1,2,3,4,5],
+    [6,7,8,9,10],
+    [11,12,13,14,15],
+    [16,17,18,19,20],
+    [21,22,23,24,25]
+]
+print(matriz_5x5[0])
+print(matriz_5x5[1])
+print(matriz_5x5[2])
+print(matriz_5x5[3])
+print(matriz_5x5[4])
+
+
+
